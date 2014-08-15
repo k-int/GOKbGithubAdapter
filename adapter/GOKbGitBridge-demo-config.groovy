83f5bb7270ba97fb0de56d62e1e9ed5a061dd618
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-15 11:10:25 +0100

    Checkout working, title iterations

---
+++ adapter/GOKbGitBridge-demo-config.groovy
@@ -0,0 +1,2 @@
+gitrepo='git@github.com:k-int/GOKbProjectFiles.git'
+gitkey='Riiiight'
--- adapter/update.groovy
+++ adapter/update.groovy
@@ -8,7 +8,8 @@
   @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0'),
   @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0'),
   @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
-  @Grab(group='com.k-int', module='goai', version='1.0.2') 
+  @Grab(group='com.k-int', module='goai', version='1.0.2'),
+  @Grab(group='org.ajoberstar', module='grgit', version='0.3.0') // Updated from 0.2.3
 ])
 
 
@@ -25,13 +26,51 @@ import org.apache.log4j.*
 import au.com.bytecode.opencsv.CSVReader
 import java.text.SimpleDateFormat
 import com.k_int.goai.*;
+import org.ajoberstar.grgit.*
+
+def config_file = new File('GOKbGitBridge-config.groovy')
+
+def config = new ConfigSlurper().parse(config_file.toURL())
+if ( ! config.maxtimestamp ) {
+  println("Intialise timestamp");
+  config.maxtimestamp = 0
+}
+
+
+println("git repository:${config.gitrepo}, max timestamp: ${config.maxtimestamp}");
+
+if ( config.gitrepo == null ) {
+  println("No configuration - please see GOKbGitBridge-demo-config.groovy, and copyedit to GOKbGitBridge-config.groovy");
+  System.exit(1)
+}
+
+
+def checkout_dir = new File('./checkout');
+
+def grgit = null
+if ( checkout_dir.exists() ) {
+  if ( checkout_dir.isDirectory() ) {
+    grgit = Grgit.open('./checkout')
+  }
+  else {  
+    System.exit(1);
+  }
+}
+else {
+  // Clone the repository
+  grgit = Grgit.clone(dir: new File('./checkout'), uri: config.gitrepo)
+}
 
 OaiClient oaiclient = new OaiClient(host:'https://gokb.k-int.com/gokb/oai/packages');
 println("Starting...");
 
 oaiclient.getChangesSince(null, 'gokb') { pkg ->
-  println("Processing package.... ${pkg}");
+  println("Processing package.... name: ${pkg.metadata.gokb.package.name.text()}");
+  // def package_file = new File("./checkout/pkg
 }
 
-
 println("Done.");
+
+config_file.withWriter { writer ->
+  config.writeTo(writer)
+}
